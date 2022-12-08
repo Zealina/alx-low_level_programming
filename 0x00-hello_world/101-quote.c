e315acca0a1560e9381ccef04fcd2ed8f01f1f44
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-08 22:36:12 +0000

    I had to check

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print to standard error
+ *
+ * Return: return one, let me see it works
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of");
+	fprintf(stderr, " art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
