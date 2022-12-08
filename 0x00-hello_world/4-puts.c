ec09a6089cb181dd7840a30b6c8bf1f727e6cb7c
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-08 19:11:49 +0000

    First ALX C Program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - use puts to print a string
+ */
+void main(void)
+{
+	char s[] = "\"Programming is like building a multilingual puzzle";
+
+	puts("%s", s);
+	return (0);
+}
