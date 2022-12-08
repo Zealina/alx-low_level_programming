af45b0415a3157f208d304dc251c451a9e07e57d
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-08 20:12:53 +0000

    Prints another string with c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string about proper grammar
+ *
+ * Return: print said string and end the program with zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
