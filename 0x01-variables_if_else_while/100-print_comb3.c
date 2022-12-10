153bdf9f3c7669bb8f6b54ced3e5880aebb981c3
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-10 13:42:44 +0000

    Print comb of single digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int c;
+	int d;
+	int a;
+	int b;
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			return (a);
+		}
+		for (d = '9'; c >= '0'; c--)
+		{
+			return (b);
+		}
+	}
+	putchar (a);
+	return (0);
+}
