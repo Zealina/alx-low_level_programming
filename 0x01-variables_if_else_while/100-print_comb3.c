674f59d5edd87c64512872104847c824f8da1e49
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-10 14:00:47 +0000

    Print double combinations of two numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - the entry point
+ * Description: Print unique combinations of double digit numbers
+ * Return: zero and end the program
+ */
 int main(void)
 {
 	int c;
 	int d;
-	int a;
-	int b;
+
+	for (c = 0; c <= 9; c++)
 	{
-		for (c = '0'; c <= '9'; c++)
+		for (d = c + 1; d <= 9; d++)
 		{
-			return (a);
-		}
-		for (d = '9'; c >= '0'; c--)
-		{
-			return (b);
-		}
-	}
-	putchar (a);
+			putchar ('0' + c);
+			putchar ('0' + d);
+			if ((c != 8) || (d != 9))
+			{
+				putchar (',');
+				putchar (' ');
+			};
+		};
+	};
+	putchar ('\n');
 	return (0);
 }
