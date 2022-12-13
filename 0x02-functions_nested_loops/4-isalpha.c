f326fb1b38cecdf52b0087f4fb70b5000f184093
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-13 19:01:30 +0000

    Check if an argument is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _islower - the entry point
+ * Description: Function to check for the case of character
+ * @c: value of the input in the function
+ * Return: int c
+ */
+#include "main.h"
+int _isalpha(int c)
+{
+	int n;
+
+	if ((c >= 65 && c <= 89) || (c >= 97 && c <= 121))
+	{
+		n = 1;
+	}
+	else
+	{
+		n = 0;
+	};
+	return (n);
+}
