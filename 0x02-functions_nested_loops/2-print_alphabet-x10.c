de1a76fc1c8541897c7a2ce0b581dbe32d190fb2
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-13 17:30:48 +0000

    Print 10 x alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet-x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - the entry point
+ * Description: Print the Alphabets usng two _putchar
+ * Return: Integer zero, End of program
+*/
+#include "main.h"
+void print_alphabet_x10(void)
+{
+	char i;
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
