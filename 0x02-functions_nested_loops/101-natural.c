ad32f4c5196e3068ee597fb328f3a7be3bfeef0e
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-15 00:39:07 +0000

    Print the sum of multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,22 @@
 void print_natural_numbers(int n)
 {
 	int a, b, c = 0;
+	int d, e = 0;
 
 	for (a = 0; a < n; a++)
 	{
-		b = (a % 3) || (a % 5);
+		b = (a % 3);
+		d = (a % 5);
 		if (b == 0)
 		{
 			c += a;
 		}
+		if (d == 0)
+		{
+			e += a;
+		}
 	}
-	printf("%d\n", c);
+	printf("%d\n", (c + e));
 }
 /**
  * main - the entry point
