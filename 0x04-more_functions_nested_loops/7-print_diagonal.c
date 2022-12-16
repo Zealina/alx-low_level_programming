0ca7c0c44fe8065780f28c44311e3dd74df10331
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-16 21:32:43 +0000

    Print diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print diagonal
+ * @n: The argument
+ * Return: Always zero
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 1; a <= n; a++)
+	{
+		for (b = 1; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
