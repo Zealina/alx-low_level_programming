a9ae64b5d6e24e80b116ec1173034b424f65bc4c
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-16 02:41:41 +0000

    Print squares with input

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using # sign
+ * @size: The argument
+ * Return: void
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,2 @@
-void print_line(int n);
+void print_square(int size);
 int _putchar(char c);
