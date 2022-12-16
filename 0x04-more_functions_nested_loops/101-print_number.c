16aa9fa0d5454464a5e7e0704e883dd548c04aed
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-16 23:32:55 +0000

    Print integers without long

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - Function to print integer
+ * @n: The argument
+ */
+void print_number(int n)
+{
+	while (n < 0)
+	{
+		n = n * (-1);
+		_putchar('-');
+	}
+	if ((n >= 0 && n < 10) || (n > -10 && n < 0))
+	{
+		_putchar('0' + n);
+	}
+	else if ((n >= 10 && n < 100) || (n > -100 && n <= -10))
+	{
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+	}
+	else if ((n >= 100 && n < 1000) || (n > -1000 && n <= -100))
+	{
+		_putchar('0' + (n / 100));
+		_putchar('0' + ((n / 10) % 10));
+		_putchar('0' + (n % 10));
+	}
+	else if ((n >= 1000 && n < 10000) || (n > -10000 && n <= -1000))
+	{
+		_putchar('0' + (n / 1000));
+		_putchar('0' + ((n / 100) % 10));
+		_putchar('0' + ((n / 10) % 10));
+		_putchar('0' + (n % 10));
+	}
+}
