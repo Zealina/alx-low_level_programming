b52f823d0ad728aa0e870c54476525767657b042
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-16 00:43:54 +0000

    Print the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,16 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if ((b > a) && (b > c))
+	else if ((b >= a) && (b >= c))
 	{
 		largest = b;
 	}
-	else if ((c > a) && (c > b))
-	{
-		largest = c;
-	}
 	else
 	{
+		largest = c;
 	}
 	return (largest);
 }
