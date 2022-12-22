d74db9e17c6047f10e5cae81bb36d8551f4f4580
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-22 15:17:57 +0000

    Captialize the worst case

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,14 @@
 char *cap_string(char *ptr)
 {
 	int x, y;
-	char checker[] = {44, 46, 123, 125, 59, 40, 41, 34, 33, 63, ' ', '\t', '\n'};
+	char checker[14] = {44, 46, 123, 125, 59,
+		40, 41, 34, 33, 63, ' ', '\t', '\n'};
 
-	for (x = 0; ptr[x]; x++)
+	for (x = 0; ptr[x] != '\0'; x++)
 	{
 		for (y = 0; checker[y] != '\0'; y++)
 		{
-			if (ptr[x + 1] >= 97 && ptr[x + 1] <= 122 && ptr[x] == checker[y])
+			if (ptr[x] == checker[y] && ptr[x + 1] >= 97 && ptr[x + 1] <= 12)
 			{
 				ptr[x + 1] -= 32;
 			}
