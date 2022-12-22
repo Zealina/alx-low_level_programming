5e2ab389f63cac5042282d750cfa8ba114605718
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-22 14:48:39 +0000

    Capitalize the worst

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - Function to capitalize first letters of a string
+ * @ptr: Pointer to the string
+ * Return: The Pointer to the string
+ */
+char *cap_string(char *ptr)
+{
+	int x, y;
+	char checker[] = {44, 46, 123, 125, 59, 40, 41, 34, 33, 63, ' ', '\t', '\n'};
+
+	for (x = 0; ptr[x]; x++)
+	{
+		for (y = 0; checker[y]; y++)
+		{
+			if (ptr[x + 1] >= 97 && ptr[x + 1] <= 122 && ptr[x] == checker[y])
+			{
+				ptr[x + 1] -= 32;
+			}
+		}
+	}
+	return (ptr);
+}
