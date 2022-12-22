0841f569734e002343006f7ee777fea200c9012a
Hamilton Sebastine <hamilsebastine@gmail.com>
2022-12-22 12:02:35 +0000

    Copy a string to a buffer

---
+++ 0x06-pointers_arrays_strings/2-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - function to copy one string to another
+ * @dest: Pointer to string 1
+ * @src: Pointer to string source
+ * @n: Number of limiting bytes
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && dest[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	for (; x < n; x++)
+	{
+		dest[x] = '\0';
+	}
+	return (dest);
+}
