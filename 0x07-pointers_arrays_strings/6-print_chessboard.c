29f53972200ff5aaa13c347df8275c09545c3d83
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-01-02 18:17:43 +0000

    Print Chessboard

--- 0x07-pointers_arrays_strings/6-print_chessboard.c
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -2,14 +2,19 @@
 
 /**
  * print_chessboard - Print the chessboard with multi-arrays
- *
+ * @a: The source of the multiarry
  * Return: Nothing, void
  */
 void print_chessboard(char (*a)[8])
 {
 	int ind1 = 0, ind2 = 0;
 
-	for (ind1 = 0; a[ind1][ind2]; ind1++)
+	for (ind1 = 0; ind1 < 8; ind1++)
 	{
-		for (ind2 = 0; a[ind1][ind2]; ind2++)
+		for (ind2 = 0; ind2 < 8; ind2++)
 		{
+			_putchar(a[ind1][ind2]);
+		}
+		_putchar('\n');
+	}
+}
