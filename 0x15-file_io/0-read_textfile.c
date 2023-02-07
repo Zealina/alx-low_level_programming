3773b7bb57207e0ea351bbd77bb227d95eeb0dd0
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-02-07 21:20:54 +0000

    First submission in a while

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile - Read a file
+ * @filename: the name of the file
+ * @letters: How many letters are to be read
+ * Return: The actual number of letters that could be printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t rdret, wrret;
+	char *buf; 
+	int fd;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	rdret = read(fd, buf, letters);
+	wrret = write(STDOUT_FILENO, buf, rdret);
+	return (wrret);
+}
