c587ef4ec8d8a83e524429f5a8616aaaaf43915c
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-02-10 22:57:23 +0000

    Make my own cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,53 +1,75 @@
 #include "main.h"
+#include <stdio.h>
+
 
 /**
- * main - a program that copies the content of one file
- * to another.
- * @ac: counter for the number of arguments
- * @av: pointer to the arguments
- * Return: 97, 98, 99 or 100 for various errors
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-int main(int ac, char **av)
-{
-	ssize_t nwr, nrd = 1024;
-	int file_f, file_t, ncl, ncl2;
-	char *buf;
 
-	if (ac != 3)
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - check the code for ALX School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	file_f = open(av[1], O_RDONLY);
-	file_t = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
-	buf = malloc(sizeof(char) * 1024);
-	while (nrd == 1024)
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		nrd = read(file_f, buf, 1024);
-		if (file_f == -1 || nrd == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
-		nwr = write(file_t, buf, nrd);
-		if (file_t == -1 || nwr == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", av[1]);
-			exit(99);
-		}
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
 	}
-	ncl = close(file_f);
-	if (ncl == -1)
+
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", ncl);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	ncl2 = close(file_t);
-	if (ncl2 == -1)
+
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
-		dprintf(2, "Error: Can't close fc %d\n", ncl2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	free(buf);
 	return (0);
 }
