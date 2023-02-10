bb4dac1a66eaccb61a0562ef88e3d6a5dcfebf8e
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-02-10 22:42:52 +0000

    My own cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
  */
 int main(int ac, char **av)
 {
-	ssize_t nwr, nrd;
+	ssize_t nwr, nrd = 1024;
 	int file_f, file_t, ncl, ncl2;
 	char *buf;
 
@@ -19,19 +19,22 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	file_f = open(av[1], O_RDONLY);
-	buf = malloc(sizeof(char) * 1024);
-	nrd = read(file_f, buf, 1024);
-	if (file_f == -1 || nrd == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	file_t = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
-	nwr = write(file_t, buf, nrd);
-	if (file_t == -1 || nwr == -1)
+	buf = malloc(sizeof(char) * 1024);
+	while (nrd == 1024)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[1]);
-		exit(99);
+		nrd = read(file_f, buf, 1024);
+		if (file_f == -1 || nrd == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		nwr = write(file_t, buf, nrd);
+		if (file_t == -1 || nwr == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[1]);
+			exit(99);
+		}
 	}
 	ncl = close(file_f);
 	if (ncl == -1)
