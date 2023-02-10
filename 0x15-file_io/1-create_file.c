35d03d3999b8efb4d6fbfa3a6f0f4f12e0cee9b9
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-02-10 11:39:17 +0000

    Truncate the file or create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	{
 		wrn = write(fd, text_content, length);
-		if (wrn != length)
+		if (wrn == -1)
 			return (-1);
 	}
 	close(fd);
