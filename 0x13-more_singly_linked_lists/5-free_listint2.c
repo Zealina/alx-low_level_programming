977a141555d26880266eb3d9dd8817db151149ad
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-02-20 03:04:17 +0000

    Free a list and set pointer to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_listint2 - frees a singly linked list and sets the head
+ *	to NULL
+ * @head: Pointer to the list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
