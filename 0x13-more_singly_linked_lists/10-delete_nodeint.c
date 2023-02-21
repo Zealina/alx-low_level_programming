d33320f46112ee9ef418f65ec2c144046f0eadd2
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-02-21 20:20:42 +0000

    Bettied 10-delete_nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (temp == NULL)
 			return (-1);
-		prev = temp; 
+		prev = temp;
 		temp = temp->next;
 	}
 	new_temp = temp->next;
