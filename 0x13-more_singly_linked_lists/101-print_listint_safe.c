8ff4be3e6f0d909321a890c9ad14578d8e23f536
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-02-24 12:07:51 +0000

    Nothing changed

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - print all the elements of the listint
+ * @head: Pointer to the list
+ *
+ * Return: The number of nodes printed
+ */
+size_t print_listint_safe(const listint_t 
