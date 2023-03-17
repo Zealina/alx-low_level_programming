a7f415aeab6fb913e328e73a6b86adbd2c96d92a
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-03-17 03:49:39 +0000

    Fixed it?

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	new->next = (*head);
+	if (*head != NULL)
+		(*head)->prev = new;
+	new->next = *head;
 	*head = new;
 	return (*head);
 }
