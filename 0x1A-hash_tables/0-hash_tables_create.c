fe583fce3cc31b64f02c6682fbae7e4b52dcc84f
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-07-26 17:32:11 +0000

    Recover deleted files

---
+++ 0x1A-hash_tables/0-hash_tables_create.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+/**
+ * hash_table_create - function to create a hash table
+ * @size: Size of the hash table
+ *
+ * Return: Pointer to the created hash table. NULL if unsuccessful
+ */
+hash_table_create_t *hash_table_create(unsigned long int size)
+
