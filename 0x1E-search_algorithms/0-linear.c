9c21c2bdf9733fd46b63711f411f55b0a7a3fa43
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-12-13 22:53:31 +0100

    Work Damnit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,15 +12,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array)
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			printf("Value checked array");
-			printf("[%d] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return (i);
-		}
+		printf("Value checked array");
+		printf("[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
