9e60d5eca854caf13837b393c5c6c9739e20505b
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-12-13 11:09:29 +0100

    Interpolation Search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -9,3 +9,26 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
+	size_t low = 0, high = size - 1, pos;
+
+	if (!array)
+		return (-1);
+	while (high != low)
+	{
+		pos = low + ((high - low) * (value - array[low]) / (array[high] - array[low]));
+		if (pos > high)
+		{
+			printf("Value checked array[%d] is out of range\n", pos);
+			return (-1);
+		}
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
+	if (value == array[low])
+		return (low);
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+/**
+ * exponential_search - Search for value in the array
+ * @array: The array to be searched
+ * @value: The value to search for
+ * @size: The size of the array
+ *
+ * Return: the index of value if successful, -1 otherwise
+ */
+int exponential_search(int *array, size_t size, int value)
+{
