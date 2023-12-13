044ba0881798f65c7cae3dd04dca6e88606dedfd
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-12-13 19:10:12 +0100

    Exponential Seatch

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -9,3 +9,23 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
+	size_t u_bound = 1, l_bound, i;
+
+	if (!array)
+		return (-1);
+	while (u_bound < size && array[u_bound] < value)
+		u_bound *= 2;
+	l_bound = u_bound / 2;
+	if (u_bound > size)
+		u_bound = size - 1;
+	for (i = l_bound; i <=  u_bound; i = (u_bound + l_bound) / 2)
+	{
+		if (array[i] == value)
+			return (i);
+		else if (value < array[i])
+			u_bound = i - 1;
+		else if (value > array[i])
+			l_bound = i + 1;
+	}
+	return (-1);
+}
