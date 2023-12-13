aa915a4f667b34996a8ef32f72637aedd218774a
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-12-13 19:25:12 +0000

    Done

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+/**
+ * advanced_binary - Search through an array for a value and return first index
+ * @array: The array to be search
+ * @size: the size of the array
+ * @value: The value to search for
+ *
+ * Return: first index of value if successful, else -1
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
